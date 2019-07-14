15065358ac8afc095fab1cee644858177b9d19f0
denzdacs <denzehldacuyan@gmail.com>
2019-07-14 15:36:42 +0800

    updated

--- php-box/prime-generator.php
+++ php-box/prime-generator.php
@@ -2,7 +2,43 @@
     /**
      * Work in progress
      */
-    $input = readline("Input Number: ");
-    $input = explode(" ",$input);
-    var_dump($input);
+
+    function getPrime() {
+        $input = readline("Input Number: ");
+        $end_set = $input;
+        $is_prime = false;
+
+        for ($to_check = $input; $toCheck <= $end_set; $to_check++) {
+            if (isPrime($to_check)) {
+                $is_prime = true;
+                echo($to_check . "\n");
+            }
+        }
+
+        if ($is_prime) {
+             getPrime();  
+        }
+    }
+
+    getPrime();
+
+    function isPrime($number) {
+        $setNumber = floor(sqrt($number));
+
+        if ( $number == 2 || $number == 1 ) {
+            return false;
+        }
+
+        for ( $compare  = 2 ; $compare <= $setNumber ; ++$compare ) {
+            if ( $number % $compare == 0 ) {
+                break;
+            }
+        }
+
+        if( $setNumber == $compare-1 ) {
+            return true;
+        }
+
+        return false;
+    }
 ?>
